752df4a5c4e2f397c2290bfa1ee2c38b8de46ef0
Yaroslav <ppdraga@mail.ru>
2020-07-26 14:00:28 +0300

    add periodic_task

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -1,9 +1,18 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
+from datetime import timedelta
 
 from celery import shared_task
+from celery.task import periodic_task
+from celery.schedules import crontab
+
 from catalog.models import GoodItem
 
+# @periodic_task(run_every=(crontab(minute='*/1')), name='my_task_name')
+@periodic_task(run_every=(timedelta(seconds=10)), name='my_task_name')
+def my_task():
+    print('my_task_print')
+    return 'my_task_return'
 
 @shared_task
 def add(x, y):
